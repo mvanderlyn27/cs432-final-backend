e3019396b132701f3c75f391ea4644446aeae44a
FancyPanda <mvanderlyn27@gmail.com>
2020-05-19 16:57:56 -0400

    fixed lots of things, removed mongo

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 var cors = require('cors');
 const port = process.env.PORT || 8080;
@@ -11,20 +10,12 @@ app.use(express.json())
 app.get('/', (req, res) => {
     return res.send("you got it!");
 });
-//gets all form data
-app.get('/getData', (req, res) => {
+//generates truth table from input
+app.post('/getTruthTable', (req, res) => {
     console.log(req.body);
-    this.loadData().then(data=> res.send(data));
+    res.send(req.body);
 });
-//saves 1 form entry to entries
-app.post('/saveData', (req, res) => {
-    console.log(req.body);
-    this.saveData(req.body).then(data=> res.send(data));
-});
-//clears form data
-app.post('/removeData', (req, res) => {
-    this.removeData().then(data=> res.send(data));
-});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
@@ -34,51 +25,3 @@ app.listen(port, () => {
 
 
 
-/* DB FUNCTIONS */
-//connects to the mongodb
-async function connect() {
-    const uri = "mongodb+srv://root:strongpassword@im-backend-rkump.mongodb.net/test?retryWrites=true&w=majority";
-    client = new MongoClient(uri);
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
-        const db = client.db("swe432-final-db");
-
-        return db, client;
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
-}
-connect().catch(console.error);
-
-//uploads one entry to entries collection in mongodb
-async function saveData(client, data) {
-    var db, client = connect();
-    if (db != null) {
-        db.collection('entries').insertOne({
-            data
-        }).then(async function (result) {
-            await client.close();
-            return (result);
-        })
-    }
-}
-//loads all data from entries collection, could be made to search for specific strings
-async function loadData(client, data) {
-    var db, client = connect();
-
-    if (db != null) {
-        var cursor = db.collection('entries').find({});
-        await client.close();
-        return cursor;
-    }
-}
-//removes all entries, could be used to remove certain ones later
-async function removeData(client, data) {
-    var db, client = connect();
-    if (db != null) {
-        db.collection('entries').remove({});
-        await client.close();
-    }
-}
